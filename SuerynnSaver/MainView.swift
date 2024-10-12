4d58e1a894f45665140de0dc67d9442cf3b59dfb
adamoadamo <adamtaylororeilly@gmail.com>
2024-10-12 14:51:14 -0400

    JSON

--- SuerynnSaver/MainView.swift
+++ SuerynnSaver/MainView.swift
@@ -1,6 +1,22 @@
 import ScreenSaver
 import AppKit
 
+class ImageCacheManager {
+    static let shared = ImageCacheManager()
+    private var imageCache: [String: NSImage] = [:]
+
+    func loadImage(named name: String) -> NSImage? {
+        if let cachedImage = imageCache[name] {
+            return cachedImage
+        }
+        if let image = NSImage(named: name) {
+            imageCache[name] = image
+            return image
+        }
+        return nil
+    }
+}
+
 class MainView: ScreenSaverView {
     private struct Character {
         var position: CGPoint
@@ -8,53 +24,48 @@ class MainView: ScreenSaverView {
         var angle: CGFloat
         var images: [NSImage]
         var currentFrame: Int = 0
-        var frameDelayCounter: Int = 0 // Counter for slowing down frame rate
+        var frameDelayCounter: Int = 0
     }
-    
+
     private var characters: [Character] = []
     private var squareSize: CGFloat = 0
     private var imageSize: CGFloat = 0
     private let speed: CGFloat = 2.0
-    private let cornerRadius: CGFloat = 20.0 // Reduced corner radius for tighter turn
-    private let frameDelay: Int = 4 // Slow down frame rate by 25%
-    private var edgeOffset: CGFloat = -5.0 // Further reduced distance from edge for a tighter fit
+    private let cornerRadius: CGFloat = 20.0
+    private let frameDelay: Int = 4
+    private var edgeOffset: CGFloat = -6.0
     private var charactersInitialized = false
-    
+    private let minDistance: CGFloat = 100.0 // Minimum distance between characters
+
     override init?(frame: NSRect, isPreview: Bool) {
         super.init(frame: frame, isPreview: isPreview)
-        loadImages()
-        animationTimeInterval = 1.0 / 30.0 // Smooth animation
+        loadImagesFromJSON()
+        animationTimeInterval = 1.0 / 30.0
     }
 
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
 
-    private func loadImages() {
-        let characterData = [
-            ("Apple", 3),
-            ("Butterfly", 8),
-            ("Man", 3),
-            ("Paper", 3),
-            ("Peanut", 3),
-            ("Snake", 4)
-        ]
-        
-        let bundle = Bundle(for: Self.self)
-        
-        for (name, frameCount) in characterData {
+    private func loadImagesFromJSON() {
+        guard let url = Bundle.main.url(forResource: "characters", withExtension: "json"),
+              let data = try? Data(contentsOf: url),
+              let animations = try? JSONDecoder().decode([String: [String]].self, from: data) else {
+            return
+        }
+
+        for (_, frames) in animations {
             var images: [NSImage] = []
-            for frame in 1...frameCount {
-                if let image = bundle.image(forResource: "\(name)_Walk-\(frame)") {
+            for frameName in frames {
+                if let image = ImageCacheManager.shared.loadImage(named: frameName) {
                     images.append(image)
-                } else {
-                    print("Failed to load image: \(name)_Walk-\(frame)")
                 }
             }
             if !images.isEmpty {
                 characters.append(Character(position: .zero, edge: 0, angle: 0, images: images))
             }
         }
+
     }
 
     private func setupCharacters() {
@@ -66,7 +77,7 @@ class MainView: ScreenSaverView {
 
         let screenWidth = bounds.width
         let screenHeight = bounds.height
-        let startPositions = generateEdgeStartPositions(screenWidth: screenWidth, screenHeight: screenHeight)
+        let startPositions = generateNonOverlappingPositions(screenWidth: screenWidth, screenHeight: screenHeight)
 
         for (index, start) in startPositions.enumerated() {
             if index < characters.count {
@@ -77,9 +88,8 @@ class MainView: ScreenSaverView {
         }
     }
 
-    private func generateEdgeStartPositions(screenWidth: CGFloat, screenHeight: CGFloat) -> [(position: CGPoint, edge: Int, angle: CGFloat)] {
+    private func generateNonOverlappingPositions(screenWidth: CGFloat, screenHeight: CGFloat) -> [(position: CGPoint, edge: Int, angle: CGFloat)] {
         var positions: [(position: CGPoint, edge: Int, angle: CGFloat)] = []
-        let minDistance: CGFloat = imageSize * 1.5 // Minimum distance to avoid overlap
 
         for _ in 0..<characters.count {
             var newPosition: CGPoint
@@ -89,21 +99,21 @@ class MainView: ScreenSaverView {
 
             repeat {
                 isValidPosition = true
-                edge = Int.random(in: 0...3) // Randomly select an edge
+                edge = Int.random(in: 0...3)
                 switch edge {
-                case 0: // Bottom edge, moving right
+                case 0:
                     let posAlongEdge = CGFloat.random(in: edgeOffset...(screenWidth - edgeOffset))
                     newPosition = CGPoint(x: posAlongEdge, y: edgeOffset)
                     angle = 0
-                case 1: // Right edge, moving up
+                case 1:
                     let posAlongEdge = CGFloat.random(in: edgeOffset...(screenHeight - edgeOffset))
                     newPosition = CGPoint(x: screenWidth - edgeOffset, y: posAlongEdge)
                     angle = CGFloat.pi / 2
-                case 2: // Top edge, moving left
+                case 2:
                     let posAlongEdge = CGFloat.random(in: edgeOffset...(screenWidth - edgeOffset))
                     newPosition = CGPoint(x: posAlongEdge, y: screenHeight - edgeOffset)
                     angle = CGFloat.pi
-                case 3: // Left edge, moving down
+                case 3:
                     let posAlongEdge = CGFloat.random(in: edgeOffset...(screenHeight - edgeOffset))
                     newPosition = CGPoint(x: edgeOffset, y: posAlongEdge)
                     angle = 3 * CGFloat.pi / 2
@@ -112,7 +122,6 @@ class MainView: ScreenSaverView {
                     angle = 0
                 }
 
-                // Check if this new position is far enough from all previous positions
                 for previousPosition in positions {
                     let distance = hypot(newPosition.x - previousPosition.position.x, newPosition.y - previousPosition.position.y)
                     if distance < minDistance {
@@ -122,16 +131,13 @@ class MainView: ScreenSaverView {
                 }
             } while !isValidPosition
 
-            // Once a valid position is found, add it to the list
             positions.append((position: newPosition, edge: edge, angle: angle))
         }
-
         return positions
     }
 
     override func draw(_ rect: NSRect) {
         super.draw(rect)
-        
         NSColor.black.setFill()
         bounds.fill()
 
@@ -146,10 +152,10 @@ class MainView: ScreenSaverView {
         context.saveGState()
         context.translateBy(x: character.position.x, y: character.position.y)
         context.rotate(by: character.angle)
-        
+
         let image = character.images[character.currentFrame]
         image.draw(in: CGRect(x: -imageSize / 2, y: 0, width: imageSize, height: imageSize))
-        
+
         context.restoreGState()
     }
 
@@ -165,7 +171,9 @@ class MainView: ScreenSaverView {
             }
         }
 
-        setNeedsDisplay(bounds)
+        DispatchQueue.main.async {
+            self.setNeedsDisplay(self.bounds)
+        }
     }
 
     private func animateCharacter(_ character: inout Character) {
@@ -178,7 +186,7 @@ class MainView: ScreenSaverView {
 
     private func moveCharacter(_ character: inout Character) {
         switch character.edge {
-        case 0: // Bottom edge, moving right (counterclockwise)
+        case 0:
             character.position.x += speed
             if character.position.x >= bounds.width - cornerRadius - edgeOffset {
                 character.angle = lerpAngle(character.angle, target: CGFloat.pi / 2, t: 0.05)
@@ -187,7 +195,7 @@ class MainView: ScreenSaverView {
                     character.angle = CGFloat.pi / 2
                 }
             }
-        case 1: // Right edge, moving up
+        case 1:
             character.position.y += speed
             if character.position.y >= bounds.height - cornerRadius - edgeOffset {
                 character.angle = lerpAngle(character.angle, target: CGFloat.pi, t: 0.05)
@@ -196,7 +204,7 @@ class MainView: ScreenSaverView {
                     character.angle = CGFloat.pi
                 }
             }
-        case 2: // Top edge, moving left
+        case 2:
             character.position.x -= speed
             if character.position.x <= cornerRadius + edgeOffset {
                 character.angle = lerpAngle(character.angle, target: 3 * CGFloat.pi / 2, t: 0.05)
@@ -205,7 +213,7 @@ class MainView: ScreenSaverView {
                     character.angle = 3 * CGFloat.pi / 2
                 }
             }
-        case 3: // Left edge, moving down
+        case 3:
             character.position.y -= speed
             if character.position.y <= cornerRadius + edgeOffset {
                 character.angle = lerpAngle(character.angle, target: 0, t: 0.05)
